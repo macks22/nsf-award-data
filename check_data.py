3743c28016e3cb16d12389b035953699df1aa376
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-07-27 16:24:30 -0400

    general purpose data checking

---
+++ check_data.py
@@ -0,0 +1,32 @@
+import sys
+import multiprocessing as mp
+
+from parse import Awards
+from num_cpus import available_cpu_count
+
+
+def check_stuff(awards, year):
+    print 'Checking year: {}'.format(year)
+    for soup in awards[year]:
+        length = len(soup.find('AwardID').text)
+        if length > 7:
+            print length
+            sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    try:
+        zipdir = sys.argv[1]
+    except IndexError:
+        print '{} <zipdir>'.format(sys.argv[0])
+        sys.exit(1)
+
+    awards = Awards(zipdir)
+    years = awards.years()
+    print 'Checking {} years.'.format(len(years))
+    cpus = available_cpu_count()
+    pool = mp.Pool(processes=cpus)
+    for year in years:
+        pool.apply_async(check_stuff, args=(awards, year))
+    pool.close()
+    pool.join()
